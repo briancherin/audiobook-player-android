d41be223e54b784c1017ba8206ce86982e966d99
Brian Cherin <brian.cherin@gmail.com>
2019-08-26 09:17:51 -0400

    Add creation function for audio storage (dependency inversion)

--- app/src/main/java/com/corson/audiobookplayer/api/Factory.java
+++ app/src/main/java/com/corson/audiobookplayer/api/Factory.java
@@ -6,12 +6,30 @@ public class Factory {
 
     private Context context;
 
+    public Factory() {
+
+    }
+
     public Factory(Context context) {
         this.context = context;
     }
 
-    public AudioManager createAudioManager() {
-        return new AmplifyAudioManager(context);
+    public AudioManager createAudioManager() throws NoContextProvidedException{
+        if (context != null) {
+            return new AmplifyAudioManager(context);
+        } else {
+            throw new NoContextProvidedException();
+        }
+    }
+
+    public AudioStore createAudioStore() {
+        return new AmplifyAudioStore();
+    }
+
+    public class NoContextProvidedException extends Exception {
+        public NoContextProvidedException () {
+            super("Must provide application context in constructor.");
+        }
     }
 
 }
